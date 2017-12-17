f7d75b1a392c9544aa0ed20761a8ffd40c4ccf73
hahnstep <stephan.hahn@gmail.com>
2017-12-17 11:51:44 +0000

    ledtool deamon added

--- ledtool.sh
+++ ledtool.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-wdir=$( cd $(dirname $BASH_SOURCE[0]) && cd .. && pwd)
+wdir=/home/pi/P4wnP1
 
 . $wdir/setup.cfg
 
  if $PIMORONI_BLINKT ; then
-   python $wdir/p4wnp1HATs/pimoroni_blinkt/ledtool.py "$1"
+   sudo python $wdir/p4wnp1HATs/pimoroni_blinkt/ledtool.py & 
  fi
--- pimoroni_blinkt/ledtool.py
+++ pimoroni_blinkt/ledtool.py
@@ -3,24 +3,60 @@
 import sys
 import time
 import blinkt
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
 
-#blinkt.set_clear_on_exit(False)
+FILE_TO_WATCH = "/tmp/blink_count"
+
 blinkt.set_brightness(0.05)
+blinkt.set_clear_on_exit(False)
 
-def main(count):
+class Watcher:
+    def __init__(self):
+        self.observer = Observer()
 
-  if count == 0:
-    blinkt.set_all(0,0,0)
-  else:
-    for i in range(count):
-       blinkt.set_pixel(i, 255, 255, 255)
-       blinkt.show()
-       time.sleep(0.05)
+    def run(self):
+        event_handler = Handler()
+        self.observer.schedule(event_handler, "/tmp", recursive=True)
+        self.observer.start()
+        try:
+            while True:
+                time.sleep(5)
+        except:
+            self.observer.stop()
+            print "Error"
 
-    time.sleep(0.3)
+        self.observer.join()
 
+class Handler(FileSystemEventHandler):
+
+    @staticmethod
+    def on_any_event(event):
+        if event.is_directory:
+            return None
+
+        elif event.event_type == 'modified':
+            if event.src_path == FILE_TO_WATCH:
+		value = open(FILE_TO_WATCH, "r").read().split("\n")[0]
+		open(FILE_TO_WATCH, "r").seek(0)
+		try:
+                	count = int(value)
+                except ValueError:
+                	count = 255 # failover
+
+		blinkt.set_all(0,0,0) # clear 
+	
+		if count == 255:
+    			blinkt.set_all(0,0,0)
+  		else:
+    			for i in range(count):
+       				blinkt.set_pixel(i, 0, 255, 255)
+       				blinkt.show()
+       				time.sleep(0.05)
+
+    			time.sleep(0.3)
+	
 
 if __name__ == "__main__":
-#        if len(sys.argv) < 1:
-#        sys.exit()
-        main(int(sys.argv[1]))
+    w = Watcher()
+    w.run()
